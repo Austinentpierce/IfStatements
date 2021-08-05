99aefa64bbff897dd966cfe11e661eee93debfef
Austin Pierce <Austinentpierce@gmail.com>
2021-08-05 15:48:13 -0400

    if and else

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 
 namespace IfStatements
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+
+            if (isMale)
+            {
+                Console.WriteLine("You are male");
+            }
+            else
+            {
+                Console.WriteLine("You are not male");
+            }
+
+
+            Console.ReadLine();
+        }
     }
-  }
 }
